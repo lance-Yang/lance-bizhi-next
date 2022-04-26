6d8b9e23b8f750ba769e1e7ec7b183028610deca
Nickyang <Nickyang@apjcorp.com>
2022-04-26 13:30:36 +0800

    add github script and bottom pagation

--- pages/index.tsx
+++ pages/index.tsx
@@ -119,24 +119,23 @@ const Home: NextPage = () => {
           </Button>
         </div>
       </div>
-      <Row style={{marginTop:20}}>
+      <Row style={{ marginTop: 20 }}>
         <Col xs={12}>
           {
-            
+            resources.length > 0 ? <nav aria-label="Page navigation example">
+              <ul className="pagination justify-content-center">
+                <li className="page-item disabled">
+                  <a className="page-link">Previous</a>
+                </li>
+                <li className="page-item"><a className="page-link" href="#">1</a></li>
+                <li className="page-item"><a className="page-link" href="#">2</a></li>
+                <li className="page-item"><a className="page-link" href="#">3</a></li>
+                <li className="page-item">
+                  <a className="page-link" href="#">Next</a>
+                </li>
+              </ul>
+            </nav> : null
           }
-          <nav aria-label="Page navigation example">
-            <ul className="pagination justify-content-center">
-              <li className="page-item disabled">
-                <a className="page-link">Previous</a>
-              </li>
-              <li className="page-item"><a className="page-link" href="#">1</a></li>
-              <li className="page-item"><a className="page-link" href="#">2</a></li>
-              <li className="page-item"><a className="page-link" href="#">3</a></li>
-              <li className="page-item">
-                <a className="page-link" href="#">Next</a>
-              </li>
-            </ul>
-          </nav>
         </Col>
       </Row>
     </div>
